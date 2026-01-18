9e503709e7521dd25fed9093bcf58c419e183fb9
SimbaMupfu <scmupfururirwa@gmail.com>
2026-01-18 18:38:26 +0200

    Create the splash screen

--- app/src/main/java/inc/sims/hustles/warpweather/MainActivity.kt
+++ app/src/main/java/inc/sims/hustles/warpweather/MainActivity.kt
@@ -6,15 +6,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import inc.sims.hustles.warpweather.core.di.DependencyContainer
+import inc.sims.hustles.warpweather.presentation.ui.SplashScreen
 import inc.sims.hustles.warpweather.presentation.ui.WeatherScreen
 import inc.sims.hustles.warpweather.presentation.viewmodel.WeatherViewModel
 import inc.sims.hustles.warpweather.presentation.viewmodel.WeatherViewModelFactory
@@ -31,11 +31,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WarpWeatherTheme {
+                var showSplash by remember { mutableStateOf(true) }
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    WeatherScreen(viewModel = viewModel)
+                    if (showSplash) {
+                        SplashScreen(
+                            onSplashFinished = { showSplash = false }
+                        )
+                    }else{
+                        WeatherScreen(viewModel = viewModel)
+                    }
                 }
             }
         }
